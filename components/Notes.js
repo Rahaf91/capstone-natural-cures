777a793b80d039ace380a997f6d48537359bb5b6
Sylvie Doumet <sylviedoumet@web.de>
2024-09-16 18:52:42 +0200

    feature/notes-css_post-it-style

--- components/Notes.js
+++ components/Notes.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import styled from "styled-components";
 
-
-export default function Notes({ onAddNote, currentRemedy, onDeleteNote,onEditNote }) {
+export default function Notes({
+  onAddNote,
+  currentRemedy,
+  onDeleteNote,
+  onEditNote,
+}) {
   const [showTextField, setShowTextField] = useState(false);
   const [noteToDelete, setNoteToDelete] = useState(null);
   const [editingNote, setEditingNote] = useState(null);
-
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -25,7 +29,7 @@ export default function Notes({ onAddNote, currentRemedy, onDeleteNote,onEditNot
       onAddNote(currentRemedy.id, newNote);
     }
     form.reset();
-    textArea.focus();
+    setShowTextField(false); // Hide the textarea after adding/editing a note
   }
 
   function handleEdit(note) {
@@ -35,7 +39,7 @@ export default function Notes({ onAddNote, currentRemedy, onDeleteNote,onEditNot
 
   return (
     <>
-      <h2>Take notes</h2>
+      <h2>Notes</h2>
       <button
         onClick={() => {
           setShowTextField(!showTextField);
@@ -48,7 +52,7 @@ export default function Notes({ onAddNote, currentRemedy, onDeleteNote,onEditNot
         <>
           <form onSubmit={handleSubmit}>
             <label htmlFor="note-input"></label>
-            <textarea
+            <StyledTextArea
               id="note-input"
               name="notes"
               defaultValue={editingNote ? editingNote.text : ""}
@@ -67,29 +71,77 @@ export default function Notes({ onAddNote, currentRemedy, onDeleteNote,onEditNot
       )}
       <ul>
         {currentRemedy.notes?.map((note) => (
-          <li key={note.id}>
-            <p>{note.text}</p>
-            <p>{note.timestamp}</p>
-
-             <button onClick={() => handleEdit(note)}>Edit</button>
-            <button onClick={() => setNoteToDelete(note.id)}>Delete</button>
-            {noteToDelete && (
-              <div>
-                <p>Are you sure you want to delete this note?</p>
-                <button
-                  onClick={() => {
-                    onDeleteNote(currentRemedy.id, note.id);
-                    setNoteToDelete(null);
-                  }}
-                >
-                  Yes
-                </button>
-                <button onClick={() => setNoteToDelete(null)}>No</button>
-              </div>
-            )}
-          </li>
+          <StyledNoteContainer key={note.id}>
+            <StyledNote>
+              <p>{note.text}</p>
+            </StyledNote>
+            <ButtonContainer>
+              <button onClick={() => handleEdit(note)}>Edit</button>
+              <button onClick={() => setNoteToDelete(note.id)}>Delete</button>
+              {noteToDelete === note.id && (
+                <div>
+                  <p>Are you sure you want to delete this note?</p>
+                  <button
+                    onClick={() => {
+                      onDeleteNote(currentRemedy.id, note.id);
+                      setNoteToDelete(null);
+                    }}
+                  >
+                    Yes
+                  </button>
+                  <button onClick={() => setNoteToDelete(null)}>No</button>
+                </div>
+              )}
+            </ButtonContainer>
+          </StyledNoteContainer>
         ))}
       </ul>
     </>
   );
 }
+
+const StyledTextArea = styled.textarea`
+  font-size: 1.4em;
+  text-align: center;
+  font-weight: bold;
+  margin: 20px;
+  width: 250px;
+  height: 230px;
+  padding: 25px 15px;
+  background: #fefabc;
+  background-color: #fefabc;
+  background-image: linear-gradient(150deg, #efec88 0%, #fefabc 100%);
+  border: 1px solid #cccccc;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+`;
+
+const StyledNoteContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+`;
+
+const StyledNote = styled.div`
+  font-size: 1.4em;
+  text-align: center;
+  font-weight: bold;
+  width: 250px;
+  height: 230px;
+  padding: 25px 15px;
+  background: #fefabc;
+  background-color: #fefabc;
+  background-image: linear-gradient(150deg, #efec88 0%, #fefabc 100%);
+  border: 1px solid #cccccc;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+  list-style-type: none;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+
+  button {
+    margin-bottom: 10px;
+  }
+`;
